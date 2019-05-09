7c854b86c9d1ad1b7c6938c1504843d201c6e012
Mohamed <Ethical.Hacker720@gmail.com>
2019-05-09 19:03:58 -0400

    Update

--- ai/train.py
+++ ai/train.py
@@ -3,12 +3,16 @@
 # Description: Train a model & save it
 
 from os import path
+from time import time
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator 
+from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 
-class Model(object):
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+
+class Model(object):
 
     def __init__(self, train_fld, valid_fld, output_file, epochs=10, batch_size=32):
         self.model = None
@@ -18,42 +22,40 @@ class Model(object):
         self.batch_size = batch_size
         self.output_file = self.get_output_path(output_file)
 
-
     def train(self):
         self.setup_model()
         self.compile()
         self.fit()
         self.save()
 
-
     def setup_model(self):
         total_classes = 2
         input_shape = (64, 64, 3)
         self.model = Sequential()
-        self.model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
+        self.model.add(Conv2D(32, (3, 3), padding='same',
+                              activation='relu', input_shape=input_shape))
         self.model.add(Conv2D(32, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
-     
+
         self.model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
         self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
-     
+
         self.model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
         self.model.add(Conv2D(64, (3, 3), activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.25))
-     
+
         self.model.add(Flatten())
         self.model.add(Dense(512, activation='relu'))
         self.model.add(Dropout(0.5))
         self.model.add(Dense(total_classes, activation='softmax'))
 
-
     def compile(self):
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) 
-        
+        self.model.compile(
+            optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     def fit(self):
         train_datagen = ImageDataGenerator(
@@ -80,17 +82,15 @@ class Model(object):
 
         self.model.fit_generator(
             training_set,
-            steps_per_epoch=4000, 
+            steps_per_epoch=4000,
             epochs=self.epochs,
             validation_data=valid_set,
-            validation_steps=800
+            validation_steps=800,
         )
-
 
     def get_output_path(self, output_path):
         output_file = output_path
         ext = path.splitext(output_file)[1]
-        file_name = path.splitext(output_file)[0]
 
         if ext != '.h5':
             output_file += '.h5'
@@ -105,14 +105,11 @@ class Model(object):
 
         return output_file
 
-
     def save(self):
         self.model.save(self.output_file)
- 
 
 
 if __name__ == '__main__':
-
 
     '''
     Dataset
@@ -125,13 +122,11 @@ if __name__ == '__main__':
             dogs_fld
     '''
 
-
     epochs = 5
     batch_size = 32
-    valid_fld = 'dataset/valid'
-    train_fld =  'dataset/train' 
+    valid_fld = 'dataset\\valid'
+    train_fld = 'dataset\\train'
     output_name = 'cat_dog_classifier'
-
 
     classifier = Model(train_fld, valid_fld, output_name, epochs, batch_size)
     classifier.train()
